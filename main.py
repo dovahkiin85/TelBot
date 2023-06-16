5efd8800fd3d8af07e1855906663f3b1fe06272d
dovahkiin85 <ilia.ostadazim85@gmail.com>
2023-06-16 21:31:10 +0330

    ver1

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+from types import NoneType
+from click import command
+
+import telebot
+import redis
+
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+count = r.get('*')
+countLen = 0
+
+bot = telebot.TeleBot('6267587812:AAEOgErtw18ePxGF6mzFw8ceAEYCD9bRUbU')
+
+li = []
+a = r.get('list')
+print(a)
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Wassup my nigga,yo i need ye name ye get me nigga? insert yer weird-ass name in the text bar, respectfully your nigga. You can also see said weird-ass names py pressing or typing /list")
+
+
+@bot.message_handler(commands=['list'])
+def echo_all(message):
+
+    bot.reply_to(message,f"here be yer list, my dear nigga \n {r.get('list')}")
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, f"{message.text} is the name you have saved, my very respected and dearly loved nigga")
+    r.set('name',f'{message.text}')
+    li.append(r.get('name'))
+    saver = r.get('list')
+    for i in li:
+        saver = saver + '\n'
+        saver = saver + i
+    r.set('list',saver)
+
+bot.infinity_polling()
+print(li)
